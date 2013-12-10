0ea20b5c91d2b493b685bb34f30333fff0eee9b9
Indika Piyasena <indika.piyasena@gmail.com>
2013-12-10 18:41:40 +1000

    baby steps

--- explore/hello.hs
+++ explore/hello.hs
@@ -1,1 +1,10 @@
-main = putStrLn "Hello, World!"
+module Main where
+
+addition a b = a + b
+u = [1..10]
+
+main :: IO ()
+main = do let z = addition 5 3
+          putStrLn $ "The result is: " ++ show z
+
+--main = putStrLn "Hello, World!"
