59fe04ba71c4f70658f11fe900de647ef9a100e3
Indika Piyasena <indika.piyasena@gmail.com>
2013-12-11 18:01:06 +1000

    starting a linear regression

--- explore/hello.hs
+++ explore/hello.hs
@@ -3,6 +3,15 @@ module Main where
 addition a b = a + b
 u = [1..10]
 
+
+-- x is the feature matrix
+-- We define x0 = 1 for convenience
+
+x = 1 : [1..10]
+
+-- n is the number of features
+let n = length x
+
 main :: IO ()
 main = do let z = addition 5 3
           putStrLn $ "The result is: " ++ show z
