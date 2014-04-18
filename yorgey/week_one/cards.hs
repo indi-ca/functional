5337258f056b1746442915bb7fce97a9da12707a
Indika Piyasena <indika.piyasena@gmail.com>
2014-04-18 19:36:11 +1000

    doubling every other number

--- yorgey/week_one/cards.hs
+++ yorgey/week_one/cards.hs
@@ -1,6 +1,12 @@
 -- credit cards
 
 
+--a = [1, 2]
+--b = [20, 40, 60]
+--result = [ x * y | x <- a, y <- b ]
+
+
+
 
 -- convert positive integers to a list of digits
 -- for 0, or negetive inputs, it should return the empty list
@@ -17,9 +23,26 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev x = reverse (toDigits x)
 
 
+-- double evyr other number beginning from the right
+-- doubleEveryOther [8,7,6,5] == [16,7,12,5]
+-- doubleEveryOther [1,2,3] == [1,4,3]
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther x
+    | length x == 1      = x
+    | length x == 2      = [2 * head x, last x]
+    | otherwise          = doubleEveryOther (init (init x)) ++ doubleEveryOther [last (init x), last x]
+
 
 --result = toDigits (-1)
-result = toDigitsRev (12345)
+
+x = toDigitsRev (12345)
+--result = doubleEveryOther x
+--result = doubleEveryOther [1, 2, 3]
+result = doubleEveryOther [1, 2, 3, 4, 5]
+
+
+
 
 main :: IO ()
 main = do let z = result
