431dc75d28c4399334570c1e2e4c6da764c554ab
Indika Piyasena <indika.piyasena@gmail.com>
2014-05-15 22:31:09 +1000

    started folds

--- yorgey/week_04_four/Folds.hs
+++ yorgey/week_04_four/Folds.hs
@@ -9,19 +9,22 @@ import Data.List
 
 
 -- Reimplement each of the following functions in a more idiomatic
--- Haskell style. Use wholemeal programming practices, breaking each function into a pipeline of incremental transformations to an entire data structure. Name your functions fun1’ and fun2’ respectively.
+-- Haskell style. Use wholemeal programming practices, breaking each function
+-- into a pipeline of incremental transformations to an entire data structure.
+-- Name your functions fun1’ and fun2’ respectively.
 
--- Hint: For this problem you may wish to use the functions iterate and takeWhile. Look them up in the Prelude documentation to see what they do.
+-- Hint: For this problem you may wish to use the functions iterate and takeWhile.
+-- Look them up in the Prelude documentation to see what they do.
 
 fun1 :: [Integer] -> Integer
-fun1 [] = 1
+fun1 []          = 1
 fun1 (x:xs)
      | even x    = (x - 2) * fun1 xs
      | otherwise = fun1 xs
 
 
 fun2 :: Integer -> Integer
-fun2 1 = 0
+fun2 1               = 0
 fun2 n  | even n     = n + fun2 (n `div` 2)
         | otherwise  = fun2 (3 * n + 1)
 
