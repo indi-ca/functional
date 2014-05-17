f6dc7e93bd25a2c523fb89edbfdfde9f20001f63
Indika Piyasena <indika.piyasena@gmail.com>
2014-05-17 21:18:50 +1000

    fun one

--- yorgey/week_04_four/Folds.hs
+++ yorgey/week_04_four/Folds.hs
@@ -22,6 +22,11 @@ fun1 (x:xs)
      | even x    = (x - 2) * fun1 xs
      | otherwise = fun1 xs
 
+-- fun1' = foldr (\x accum -> if (even x) then (accum * (x - 2)) else accum) 1
+--fun1' =  foldl1 (*) . map (\x -> if (even x) then (x - 2) else 1)
+fun1' :: [Integer] -> Integer
+fun1' = foldl1 (*) . map (\x -> x - 2) . takeWhile (even)
+
 
 fun2 :: Integer -> Integer
 fun2 1               = 0
@@ -29,3 +34,32 @@ fun2 n  | even n     = n + fun2 (n `div` 2)
         | otherwise  = fun2 (3 * n + 1)
 
 
+
+a = [1, 1, 1]
+b = [1, 2, 3]
+c = [2, 4, 8]
+
+-- Confirmed. Sum can be implemented with both a left and right fold
+sum' = foldl1 (\accum x -> accum + x)
+sum'' = foldr1 (\x accum -> accum + x)
+
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 (\accum x -> if accum > x then accum else x)
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' = foldr1 (\x accum -> if accum > x then accum else x)
+
+-- EXERCISE 2
+
+--Recall the definition of a binary tree data structure. The height of
+--a binary tree is the length of a path from the root to the deepest node. For example, the height of a tree with a single node is 0; the height of a tree with three nodes, whose root has two children, is 1; and so on. A binary tree is balanced if the height of its left and right subtrees differ by no more than 1, and its left and right subtrees are also balanced.
+
+
+-- You should use the following data structure to represent binary trees. Note that each node stores an extra Integer representing the height at that node.
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+-- For this exercise, write a function
