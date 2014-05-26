c750ed0ca00122c5b73084d68476e3068f0cdff3
Indika Piyasena <indika.piyasena@gmail.com>
2014-05-26 22:06:03 +1000

    folds: slightly more wholemealish

--- yorgey/week_04_four/Folds.hs
+++ yorgey/week_04_four/Folds.hs
@@ -43,46 +43,16 @@ fun2 1               = 0
 fun2 n  | even n     = n + fun2 (n `div` 2)
         | otherwise  = fun2 (3 * n + 1)
 
---fun2' 1 = 0
---fun2' n = n + fun2'(n `div` 2)
 
-
-
-
--- Map Generator
-
--- the recursive call is happening deeper than the infinite list
-inf = [2..]
-g x = x + g (x `div` 2)
-even_gen = map g inf
-
-
--- Partial Applications
-
-even_nator :: Integer -> (Integer -> Integer) -> Integer
-even_nator n f = n + f (n `div` 2)
-
-odd_nator :: Integer -> (Integer -> Integer) -> Integer
-odd_nator n f = 0 + f (3 * n + 1)
-
-partially :: (Integer -> Integer) -> Integer
-partially = even_nator 2
-
-decider :: Integer -> (Integer -> Integer) -> Integer
-decider x = if even x then even_nator x else odd_nator x
-
---repeated_applications = iterate decider
-
-
-
--- Repeated Applications of the same function
-
+ --(1+) . (3*)
 f :: Integer -> Integer
 f x = if even x then (x `div` 2) else (3 * x + 1)
+fun2' =  foldl (\acc x -> if even x then acc + x else acc) 0 . takeWhile (>1) . iterate f
 
-do_it x = (takeWhile (>1) (iterate f x))
-temp = do_it 13
-more = foldl (\acc x -> if even x then acc + x else acc) 0 temp
+
+
+
+
 
 a = [1, 1, 1]
 b = [1, 2, 3, 4, 5, 6, 7]
@@ -124,7 +94,7 @@ data Tree a = Leaf
 -- For this exercise, write a function
 
 
-foldTree :: [a] -> Tree a
+--foldTree :: [a] -> Tree a
 
 
 
