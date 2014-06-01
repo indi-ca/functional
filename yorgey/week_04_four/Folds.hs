38d11c51487fd0af5411cad27d4238bfda7f8000
Indika Piyasena <indika.piyasena@gmail.com>
2014-06-01 22:56:54 +1000

    uncurrying

--- yorgey/week_04_four/Folds.hs
+++ yorgey/week_04_four/Folds.hs
@@ -1,8 +1,9 @@
 -- CIS 194 Homework 4
-
 module Folds where
-
 import Data.List
+
+
+
 
 
 -- EXERCISE 1
@@ -23,7 +24,6 @@ fun1 (x:xs)
      | otherwise = fun1 xs
 
 
-
 -- This is wrong
 fun1' = foldl1 (*) . map (\x -> x - 2) . takeWhile (even)
 -- fun1' = foldr (\x accum -> if (even x) then (accum * (x - 2)) else accum) 1
@@ -38,13 +38,12 @@ fun1'' = foldl (\accum x -> if even x then accum * (x-2) else accum) 1
 
 
 
+
+
 fun2 :: Integer -> Integer
 fun2 1               = 0
 fun2 n  | even n     = n + fun2 (n `div` 2)
         | otherwise  = fun2 (3 * n + 1)
-
-
-
 
 -- Returns
 -- Bool -> Integer -> Integer
@@ -63,11 +62,6 @@ fun2' =  foldl (\acc x -> if even x then acc + x else acc) 0 . takeWhile (>1) . 
 
 
 
-
-a = [1, 1, 1]
-b = [1, 2, 3, 4, 5, 6, 7]
-c = [2, 4, 8, 3, 4, 2, 6]
-d = [10, 7, 8, 6, 4, 3, 6, 7]
 
 
 
@@ -110,37 +104,7 @@ data Tree a = Leaf
 -- which generates a balanced binary tree from a list of values using foldr.
 
 
-
 tr = "ABCDEFGHIJ"
-
--- bascially, the smaller letter is a child
--- is this important?
-
--- how do I do this?
--- start with the first node as the parent, and create a balanced tree under it
--- simple
-
-
--- STARTEGY A: Create a balanced tree
-
----- These functions replaces the current tree with a new one
---insertLeft :: Node -> Tree -> Tree
---insertLeft _ Leaf = error "Cannot insert left into a leaf"
---insertLeft new (Node _ m right) = Node (Node Leaf new Leaf) m right
-
---insertRight :: Node -> Tree -> Tree
---insertRight _ Leaf = error "Cannot insert right into a leaf"
---insertRight new (Node left m _) = Node left m (Node Leaf new Leaf)
-
-
--- STARTEGY B: Left, right, left, right
-
--- L, R,  LL, LR, RL, RR,  LLL, LLR, LRL, LRR, RLL, RLR, RRL, RRR
--- These look like permutations
--- Not quite
-
-
--- STARTEGY C: Recursive insert into unbalanced position
 
 tree_a = Node 0 Leaf "A" Leaf
 tree_b = Node 0 Leaf "B" Leaf
@@ -156,6 +120,22 @@ tree_bacd = insertTree tree_d tree_bac
 tree_bacde = insertTree tree_e tree_bacd
 tree_bacdef = insertTree tree_f tree_bacde
 tree_bacdefg = insertTree tree_g tree_bacdef
+
+
+
+
+-- STARTEGY A: Create a balanced tree
+
+
+-- STARTEGY B: Left, right, left, right
+
+-- L, R,  LL, LR, RL, RR,  LLL, LLR, LRL, LRR, RLL, RLR, RRL, RRR
+-- These look like permutations
+-- Not quite
+
+
+-- STARTEGY C: Recursive insert into unbalanced position
+
 
 
 -- treeHeight, also disperses - and it is amazing that it comes to a conclusion
@@ -205,8 +185,12 @@ foldTree :: [a] -> Tree a
 foldTree = foldr1 (\x acc -> insertTree x acc) . map (\x -> Node 0 Leaf x Leaf)
 
 
-
 -- NEXT: A foldr implemention of insertTree
+
+
+
+
+
 
 
 
@@ -227,6 +211,12 @@ map' f = foldr (\x acc -> f x : acc) []
 
 
 
+
+
+
+
+
+
 -- EXERCISE 4:
 
 -- Implement the algorithm using function composition.
@@ -235,68 +225,17 @@ map' f = foldr (\x acc -> f x : acc) []
 cartProd :: [a] -> [b] -> [(a, b)]
 cartProd xs ys = [(x,y) | x <- xs, y <- ys]
 
-
-
--- start with a list of integers from 1 to n
-source n = [1..n :: Integer]
-
-
---do_something = cartProd (source 9) (source 9)
-initial = replicate 2 . (\x -> [1..x::Integer])
-
-
--- [?] I'm creating the matrix like this because I don't know how to apply twice
-matrix n = cartProd (source n) (source n)
-
-
-
-
--- Remove all the values where i > j
-filter_cart :: [(Integer, Integer)] -> [(Integer, Integer)]
-filter_cart = filter (\(i,j) -> i <= j)
-
-cartman x          = cartProd (source x) (source x)
-
--- Now apply a multiplication on each element
-do_mult = foldr (\(i,j) acc -> i + j + 2 * i * j : acc) []
-
--- Remove all the elements greater than: n
-
-post_filter xs n = filter (\x -> x <= n) xs
-
-
-cm = do_mult (filter_cart $  matrix 10)
-dm = post_filter cm 10
-em = remove_items (source 10) dm
-
--- now remove the items
--- i could use a list comprehension, or a foldr
---remove_items xs ys = [x | x <- xs, y <- ys, not (elem y xs)]
---remove_items xs ys = [x | x <- xs, y <- ys]
-
-remove_items :: [Integer] -> [Integer] -> [Integer]
-remove_items xs subs = foldr (\x acc -> if (elem x subs) then acc else x : acc) [] xs
-
-
-
-double_and_add_one = map (\x -> x * 2 + 1)
-
--- [?] How do I do apply twice?
+-- [?] How do I source the input right in the middle?
+-- [?] Can I over-apply?
 --sieveSundaram :: Integer -> [Integer]
---sieveSundaram n =  map (\x -> x * 2 + 1) . eliminate' . filter (\x -> x <= n) . do_mult . (filter_cart (cartman n))
---    where eliminate'         = eliminate (source n)
---          eliminate xs subs  = foldr (\x acc -> if (elem x subs) then acc else x : acc) [] xs
---          do_mult            = foldr (\(i,j) acc -> i + j + 2 * i * j : acc) []
---          cartman x          = cartProd (source x) (source x)
---          source n           = [1..n :: Integer]
-
---something n = filter_cart cartman
---    where a = 1
---          cartman n          = cartProd (source n) (source n)
---          source n           = [1..n :: Integer]
-
-sieveSundaram n = map (\x -> x * 2 + 1) . eliminate' . filter (\x -> x <= n) . do_mult . filter_cart . cartman
-    where eliminate'         = eliminate (source n)
+sieveSundaram n = map (\x -> x * 2 + 1) . eliminate' . do_mult . filter_cart . uncurry cartProd . source'
+    where eliminate'         = eliminate [1..n]
           eliminate xs subs  = foldr (\x acc -> if (elem x subs) then acc else x : acc) [] xs
+          do_mult            = foldr (\(i,j) acc -> i + j + 2 * i * j : acc) []
+          filter_cart        = filter (\(i,j) -> i <= j)
+          source'            = (\x -> (x, x)) . (\x -> [1..x])
 
-
+-- Cannot apply a tuple of three into cartProd
+overapply = uncurry cartProd . source'
+    where
+          source'            = (\x -> (x, x)) . (\x -> [1..x])
