b2a4ef4a9a9d667863bc6cc0a4bdc2a998d9d41e
Indika Piyasena <indika.piyasena@gmail.com>
2014-06-02 19:16:24 +1000

    pipelining folds

--- yorgey/week_04_four/Folds.hs
+++ yorgey/week_04_four/Folds.hs
@@ -23,19 +23,8 @@ fun1 (x:xs)
      | even x    = (x - 2) * fun1 xs
      | otherwise = fun1 xs
 
-
--- This is wrong
-fun1' = foldl1 (*) . map (\x -> x - 2) . takeWhile (even)
--- fun1' = foldr (\x accum -> if (even x) then (accum * (x - 2)) else accum) 1
---fun1' =  foldl1 (*) . map (\x -> if (even x) then (x - 2) else 1)
 fun1' :: [Integer] -> Integer
-
-
--- This is right, but it is not that wholemealish
-fun1'' = foldl (\accum x -> if even x then accum * (x-2) else accum) 1
-
-
-
+fun1' = foldl1 (*) . map (\x -> x - 2) . filter (even)
 
 
 
@@ -45,7 +34,7 @@ fun2 1               = 0
 fun2 n  | even n     = n + fun2 (n `div` 2)
         | otherwise  = fun2 (3 * n + 1)
 
-
+fun2' :: Integer -> Integer
 fun2' = sum . filter (even) . takeWhile (>1) . iterate z
     where
         z = j . k
