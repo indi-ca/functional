92a9518d8d6453a643eff11a0ba754ca0bc2b1b4
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-06-11 20:22:41 +1000

    calc: progress

--- yorgey/week_05_five/Calc.hs
+++ yorgey/week_05_five/Calc.hs
@@ -6,6 +6,10 @@ import ExprT
 
 -- EXERCISE 1
 
---Write Version 1 of the calculator: an evaluator for ExprT, with the signature
---For example, eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20.
+-- This type is capable of representing expressions involving integer constants, addition, and multiplication. For example, the expression (2 + 3) × 4 would be represented by the value
+-- Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+
+-- Write Version 1 of the calculator: an evaluator for ExprT, with the signature
+-- For example, eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20.
+
 eval :: ExprT -> Integer
