fbcd964f1e9307a594a982c3a90f118dd4ae23ec
Indika Piyasena <indika.piyasena@gmail.com>
2014-06-27 21:43:51 +1000

    playing with shapes

--- explore/shapes.hs
+++ explore/shapes.hs
@@ -15,6 +15,12 @@ class Shape a where
 instance Shape Square where
     area (Square x) = fromIntegral x * fromIntegral x
 
+instance Shape Circle where
+    area (Circle r) = 2 * pi * fromIntegral (r * r)
 
+instance Shape Triangle where
+    area (Triangle x) = fromIntegral (x * x) / 2
 
-square_one = Square 8
+square_one = Square 5
+circle_one = Circle 5
+triangle_one = Triangle 5
