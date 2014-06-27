15d4a9dcc7fd22d049034e187b98b005688adb47
Indika Piyasena <indika.piyasena@gmail.com>
2014-06-27 23:16:58 +1000

    playing with shapes

--- explore/shapes.hs
+++ explore/shapes.hs
@@ -1,14 +1,19 @@
-
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 -- How do I give these aliases?
+-- What I want is a Type synonym
+-- Like this
 
-data Square = Square Integer deriving (Show)
-data Circle = Circle Integer deriving (Show)
-data Triangle = Triangle Integer deriving (Show)
+type Radius = Integer
+
+data Square = Square Integer deriving (Eq, Ord, Show)
+data Circle = Circle Radius deriving (Eq, Ord, Show)
+data Triangle = Triangle Integer deriving (Eq, Ord, Show)
 
 
+-- a default implementation seems impossible
 class Shape a where
-
     area :: a -> Float
 
 
@@ -21,6 +26,32 @@ instance Shape Circle where
 instance Shape Triangle where
     area (Triangle x) = fromIntegral (x * x) / 2
 
+-- Instances of collections can also be defined
+instance Shape a => Shape [a] where
+    area = sum . map area
+
+instance (Shape a, Shape b) => Shape (a, b) where
+    area (a, b) = area a + area b
+
+-- What about either or
+--type SquareOrCircle = Either Square Circle
+instance (Shape a, Shape b) => Shape (Either a b) where
+    area (Left a) = area a
+    area (Right b) = area b
+
+
+
 square_one = Square 5
 circle_one = Circle 5
 triangle_one = Triangle 5
+
+shapes = [Square 5, Square 15, Square 3]
+
+-- So, now, how can I operate on a type that I don't know
+-- Suppose I have another function
+findArea :: (Shape a) => a -> Float
+findArea shape = area shape
+
+
+sortByArea :: Shape a => [a] -> [a]
+sortByArea = sortBy (comparing area)
