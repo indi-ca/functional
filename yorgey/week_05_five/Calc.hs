1ec4fa941764880df7a27ed2bd5cffa40f478879
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-07-14 21:05:32 +1000

    calc: variables

--- yorgey/week_05_five/Calc.hs
+++ yorgey/week_05_five/Calc.hs
@@ -6,7 +6,7 @@ module Calc where
 import ExprT
 import Parser
 import StackVM
-
+import qualified Data.Map as M
 
 
 -- EXERCISE 1
@@ -272,14 +272,67 @@ compile arithmetic = expression :: Maybe Program
 -- Some users of your calculator have requested the ability to give
 -- names to intermediate values and then reuse these stored values later.
 
+-- To enable this, you first need to give arithmetic expressions the ability to contain variables.
+-- Create a new type class HasVars a which contains a single method var :: String -> a.
+-- Thus, types which are instances of HasVars have some notion of named variables.
 
--- To enable this, you first need to give arithmetic expressions the ability to contain variables. Create a new type class HasVars a which contains a single method var :: String -> a. Thus, types which are instances of HasVars have some notion of named variables.
+class HasVars a where
+    var :: String -> a
 
--- Start out by creating a new data type VarExprT which is the same as ExprT but with an extra constructor for variables. Make VarExprT an instance of both Expr and HasVars. You should now be able to write things like
+-- Start out by creating a new data type VarExprT which is the same as ExprT but with an extra constructor for variables.
+-- Make VarExprT an instance of both Expr and HasVars.
+
+-- How do I do this without specifying a new module?
+
+data VarExprT = Lit2 Integer
+            | Add2 VarExprT VarExprT
+            | Mul2 VarExprT VarExprT
+            | Var VarExprT
+
+-- You should now be able to write things like
 -- *Calc> add (lit 3) (var "x") :: VarExprT
 
--- But we can’t stop there: we want to be able to interpret expres- sions containing variables, given a suitable mapping from variables to values. For storing mappings from variables to values, you should use the Data.Map module. Add
--- import qualified Data.Map as M
--- at the top of your file. The qualified import means that you must prefix M. whenever you refer to things from Data.Map. This is stan- dard practice, since Data.Map exports quite a few functions with names that overlap with names from the Prelude. Consult the Data.Map documentation to read about the operations that are sup- ported on Maps.
+-- But we can’t stop there: we want to be able to interpret expres-sions containing variables,
+-- given a suitable mapping from variables to values.
 
+-- For storing mappings from variables to values, you should use the Data.Map module. Add
+-- import qualified Data.Map as M
+-- at the top of your file.
+-- The qualified import means that you must prefix M. whenever you refer to things from Data.Map.
+-- This is standard practice, since Data.Map exports quite a few functions with
+-- names that overlap with names from the Prelude.
+-- Consult the Data.Map documentation to read about the operations that are supported on Maps.
+
+-- Implement the following instances:
+--  instance HasVars (M.Map String Integer -> Maybe Integer)
+--  instance Expr (M.Map String Integer -> Maybe Integer)
+
+-- The first instance says that variables can be interpreted as
+-- functions from a mapping of variables to Integer values to (possibly) Integer values.
+-- It should work by looking up the variable in the mapping.
+-- The second instance says that these same functions can be interpreted as expressions
+-- (by passing along the mapping to subexpressions and combining results appropriately).
+
+-- Note: to write these instances you will need to enable the FlexibleInstances language extension by putting
+-- {-# LANGUAGE FlexibleInstances #-}
+
+--Once you have created these instances, you should be able to test them as follows:
+
+-- withVars :: [(String, Integer)]
+--          -> (M.Map String Integer -> Maybe Integer)
+--          -> Maybe Integer
+
+-- withVars vs exp = exp $ M.fromList vs
+
+-- *Calc> :t add (lit 3) (var "x")
+-- add (lit 3) (var "x") :: (Expr a, HasVars a) => a
+
+-- *Calc> withVars [("x", 6)] $ add (lit 3) (var "x")
+-- Just 9
+
+-- *Expr> withVars [("x", 6)] $ add (lit 3) (var "y")
+-- Nothing
+
+-- *Calc> withVars [("x", 6), ("y", 3)]
+--Just 54
 
