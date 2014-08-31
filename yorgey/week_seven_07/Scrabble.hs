6c988454d1732b793f2fdd5e7de4db7af807ce8e
Indika Piyasena <indika.piyasena@gmail.com>
2014-08-31 17:53:12 +1000

    scrabble: implemented score functions

--- yorgey/week_seven_07/Scrabble.hs
+++ yorgey/week_seven_07/Scrabble.hs
@@ -1,21 +1,47 @@
 module Scrabble where
 
 import Data.Char (toUpper, isLetter)
-
+import Data.Maybe (fromJust)
+import Data.Monoid
 
 -- EXCERCISE 3
 
 -- Hence, the second annotation you decide to implement is one to
 -- cache the ScrabbleTM score for every line in a buffer.
 -- Create a Scrabble module that defines a Score type, a Monoid instance for Score,
--- and the following functions:
 
+-- a Score type
+data Score = Score Int
+    deriving (Show)
+
+instance Monoid Score where
+    mempty = Score 0
+    mappend (Score x) (Score y) = Score (x + y)
+
+
+-- and the following functions:
 -- score :: Char -> Score
 -- scoreString :: String -> Score
 
 -- The score function should implement the tile scoring values as shown at
 -- http://www.thepixiepit.co.uk/scrabble/rules.html
 -- any characters not mentioned (punctuation, spaces, etc.) should be given zero points.
+
+score :: Char -> Score
+score x
+    | not $ isLetter x = Score 0
+    | otherwise = Score (fromJust $ lookup (toUpper x) (zip letters points))
+    where
+        letters = ['A'..'Z']
+        points = [1, 3, 3, 2, 1, 4, 2, 4,
+                  1, 8, 5, 1, 3, 1, 1, 3,
+                  10, 1, 1, 1, 1, 4, 4, 8,
+                  4, 10]
+
+scoreString :: String -> Score
+scoreString = mconcat . map score
+
+
 -- To test that you have everything working, add the line import Scrabble to the
 -- import section of your JoinList module,
 -- and write the following function to test out JoinLists annotated with scores:
@@ -27,22 +53,6 @@ import Data.Char (toUpper, isLetter)
 --        (Single (Score 9) "yay ")
 --        (Single (Score 14) "haskell!")
 
-
---score :: Char -> Score
-
-
--- a Score type
-data Score = Score Int
-
-
---score :: Char -> Score
-score x
-    | not $ isLetter x = 0
-    | otherwise = points !! y
-    where
-        y = digitToInt x - 10
-        letters = ['A'..'Z']
-        points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 8, 4, 10]
 
 
 
