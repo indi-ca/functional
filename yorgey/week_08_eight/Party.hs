f46751b74bae4f0941592a78ec405750a32f87c4
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-09-15 23:36:04 +1000

    party: style

--- yorgey/week_08_eight/Party.hs
+++ yorgey/week_08_eight/Party.hs
@@ -11,22 +11,6 @@ import Employee
 
 
 
--- maximise the amount of fun
--- determine who to invite
-
--- Employee names are represented by Strings.
--- type Name = String
-
--- The amount of fun an employee would have at the party, represented
--- by an Integer
--- type Fun  = Integer
-
--- An Employee consists of a name and a fun score.
--- data Employee = Emp { empName :: Name, empFun :: Fun }
---   deriving (Show, Read, Eq)
-
-
--- testCompany :: Tree Employee
 
 -- Guest List
 -- Obvious way would be [Employee]
