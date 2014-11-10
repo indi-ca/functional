1725a4315e42f2f2ae10be980d10003e801b04bd
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-11-10 23:30:45 +1000

    doing a liftIO

--- explore/json/LearnHedis.hs
+++ explore/json/LearnHedis.hs
@@ -1,22 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
 
---module LearnHedis where
+module LearnHedis where
+
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Functor
+import Control.Applicative
 
 import Database.Redis
 
 
---redisThing :: Connection -> IO (Either Reply Status)
---redisThing conn = conn $ do
---    set "hello" "hello"
-
-
 main :: IO ()
 main = do
+    putStrLn "Connecting with Redis"
+
     -- connects to localhost:6379
     conn <- connect defaultConnectInfo
     runRedis conn $ do
-        set "hello" "hello"
+        set "yo" "yo"
+        ret <- incr "ia:count"
+        liftIO $ print (ret)
+        set "yo" "yo"
 
-    putStrLn "Connecting with Redis"
+    putStrLn "Done"
 
 
+render :: [Integer] -> String
+render (x:xs) = show x
+
+
+--doSomething :: Connection -> IO ()
+--doSomething conn = runRedis conn $ do
+--    set "bob" "bob"
+--    putStrLn "Done"
+
+--makeKey :: Int -> String
+--makeKey index = "ia" ++ ":" ++ index
