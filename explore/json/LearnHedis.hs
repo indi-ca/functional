793ff9f02db724161f40e966b239181fa9cb8868
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-11-12 22:59:29 +1000

    playing with hedis

--- explore/json/LearnHedis.hs
+++ explore/json/LearnHedis.hs
@@ -8,7 +8,8 @@ import Data.Functor
 import Control.Applicative
 
 import Database.Redis
-
+import Data.ByteString.Char8(pack, unpack)
+import Data.ByteString.Internal(ByteString)
 
 main :: IO ()
 main = do
@@ -22,6 +23,8 @@ main = do
         liftIO $ print (ret)
         set "yo" "yo"
 
+    doRedisCreate conn
+
     putStrLn "Done"
 
 
@@ -29,10 +32,48 @@ render :: [Integer] -> String
 render (x:xs) = show x
 
 
---doSomething :: Connection -> IO ()
---doSomething conn = runRedis conn $ do
---    set "bob" "bob"
---    putStrLn "Done"
+doRedisCreate :: Connection -> IO (Either Reply Status)
+doRedisCreate conn = runRedis conn $ do
+    ret <- incr "ia:count"
+
+    let key :: ByteString
+        yes_no = dealWithTheEither ret
+        key = pack ("ia:" ++ (show (ret)))
+    liftIO $ putStrLn (show yes_no)
+    set key "value"
+    set "sally" "yo"
+
+
+doRedisIncrement :: Connection -> IO (Either Reply Integer)
+doRedisIncrement conn = runRedis conn $ do
+    ret <- incr "ia:count"
+    return ret
+
+doRedisSet :: Connection -> ByteString -> ByteString -> IO (Either Reply Status)
+doRedisSet conn key value = runRedis conn $ do
+    ret <- set key value
+    return ret
+
+createKey :: Integer -> ByteString
+createKey x = pack ("ia:" ++ show x )
+
+
+dealWithTheEither :: Either Reply Integer -> Bool
+dealWithTheEither (Left _) = False
+dealWithTheEither (Right x) = True
+
+
+theThingThatCalls :: Connection -> IO (Bool)
+theThingThatCalls conn = do
+    return True
+
+-- What do I want?
+-- I want to combine two IO functions
+-- Call the second, if the first one succeeds, and return True
+-- Or just return False
+
+
+
 
 --makeKey :: Int -> String
 --makeKey index = "ia" ++ ":" ++ index
