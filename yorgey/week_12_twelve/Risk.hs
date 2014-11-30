3a0c3330bf317fc298307f8718383d240fc321e5
Indika Piyasena <indika.piyasena@gmail.com>
2014-11-30 22:19:50 +1000

    risk: playing with dice

--- yorgey/week_12_twelve/Risk.hs
+++ yorgey/week_12_twelve/Risk.hs
@@ -7,7 +7,7 @@ import Control.Monad.Random
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
+newtype DieValue = DV { unDV :: Int }
   deriving (Eq, Ord, Show, Num)
 
 first :: (a -> b) -> (a, c) -> (b, c)
@@ -26,3 +26,18 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+
+--The die function simulates the roll of a die, picking a number between 1 and 6, inclusive, and returning it in the Rand monad. Notice that this code will work with any source of random numbers g.
+
+die' :: (RandomGen g) => Rand g Int
+die' = getRandomR (1,6)
+--The dice function uses replicate and sequence to simulate the roll of n dice.
+
+dice :: (RandomGen g) => Int -> Rand g [Int]
+dice n = sequence (replicate n die')
+--To extract a value from the Rand monad, we can can use evalRandIO.
+
+main = do
+  values <- evalRandIO (dice 2)
+  putStrLn (show values)
