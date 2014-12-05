6558e90c6a0a2d18087f3502e1dfa452bc05c2ac
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-12-05 18:50:07 +1000

    monad: play

--- yorgey/week_12_twelve/PlayMonad.hs
+++ yorgey/week_12_twelve/PlayMonad.hs
@@ -8,7 +8,7 @@ import Control.Monad
 
 --import Data.Maybe hiding ((>>=), return)
 data Maybe' a = Nothing' | Just' a
-
+    deriving Show
 
 instance Functor Maybe' where
     fmap _ Nothing' = Nothing'
@@ -28,3 +28,28 @@ instance Monad Maybe' where
 
     -- >>= :: m a -> (a -> m b) -> m b
     (Just' x) >>= k = k x
+
+
+
+check :: Int -> Maybe' Int
+check n
+    | n > 10    = Just' n
+    | otherwise = Nothing'
+
+
+--halve :: Int -> Maybe' Int
+--halve x = x / 2
+
+
+b = return 3 :: Maybe' Int
+c = return 11 :: Maybe' Int
+
+
+
+-- How do I override the default implementation of the instance
+newtype SpawnList a = SpawnList [a]
+    deriving (Eq, Show, Functor)
+
+--instance Monad [] where
+--    return x = [x]
+
--- yorgey/week_12_twelve/Risk.hs
+++ yorgey/week_12_twelve/Risk.hs
@@ -20,15 +20,10 @@ instance Random DieValue where
 die :: Rand StdGen DieValue
 die = getRandom
 
-------------------------------------------------------------
--- Risk
-
-type Army = Int
-
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
 
 
---The die function simulates the roll of a die, picking a number between 1 and 6, inclusive, and returning it in the Rand monad. Notice that this code will work with any source of random numbers g.
+-- The die function simulates the roll of a die, picking a number between 1 and 6, inclusive,
+-- and returning it in the Rand monad. Notice that this code will work with any source of random numbers g.
 
 die' :: (RandomGen g) => Rand g Int
 die' = getRandomR (1,6)
@@ -41,3 +36,80 @@ dice n = sequence (replicate n die')
 main = do
   values <- evalRandIO (dice 2)
   putStrLn (show values)
+
+
+------------------------------------------------------------
+-- Risk
+
+type Army = Int
+
+data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+
+
+-- EXCERCISE 2
+
+-- write a function of this type
+
+-- What does this do?
+-- Which creates the result of a random battle
+--battle :: Battlefield -> Rand StdGen Battlefield
+
+
+-- Now I know, how to create the result of a die roll
+
+
+die_roll :: (RandomGen g) => Rand g Int
+die_roll = getRandomR (1, 6)
+
+die_roll' :: Rand StdGen Int
+die_roll' = getRandomR (1, 6)
+
+
+
+-- Rand is, apparently a Monad,
+foo :: Int -> Rand StdGen Int
+foo x = return x
+
+
+
+-- therefore, I can call return on it
+
+--bob ::  Int
+--bob = return die_roll'
+
+-- I can also bind them together
+--something = die_roll' (>>=) die_roll'
+
+
+
+
+
+
+
+-- What are the rules of the battle?
+-- There is the attacker, and there is the defender
+
+--instance Monad Rand where
+--    --return
+--    ma (>>=) mb =
+
+-- What does it mean to derive something
+
+
+
+-- Cannot do this
+--data Apple a = Apple Int a
+--    deriving (Monad)
+
+
+
+
+
+
+
+
+
+
+
+
