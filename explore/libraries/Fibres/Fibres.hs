d4bde1caceba23a7d9c4e697e56d1061dcc288b6
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-12-31 02:38:18 +1000

    hedis: very complex way of ensuring that there is one item in the the index list

--- explore/libraries/Fibres/Fibres.hs
+++ explore/libraries/Fibres/Fibres.hs
@@ -1,14 +1,17 @@
 module Fibres where
 
 
-import Network.HTTP
+import Network.HTTP(simpleHTTP, getRequest, getResponseBody)
 import System.Directory(getTemporaryDirectory)
 import System.FilePath.Posix((</>))
 import System.IO(hPutStrLn, openFile, hClose, Handle, IOMode( WriteMode ))
 
-import Database.Redis
+import Database.Redis(Redis, Connection, Reply, runRedis, connect, defaultConnectInfo, hset, lindex, rpush)
 
+import Data.ByteString.Char8(pack, unpack)
+import Control.Monad.IO.Class(liftIO)
 
+import Data.ByteString.Internal as Foo(ByteString)
 
 type URL = String
 
@@ -17,6 +20,45 @@ data Search = Search {
     keyword :: String,
     url :: URL
 } deriving Show
+
+
+
+--writeSomething :: String -> IO (Either Reply Integer)
+
+main = do
+    conn <- connect defaultConnectInfo
+    writeSomething conn
+    runRedis conn $ do
+        ret <- hset (pack "test") (pack "hello") (pack "value")
+        liftIO $ print ("")
+    -- ret <- sadd (pack "key") [pack "subkey"] (pack "value")
+    putStrLn "Done"
+
+
+--writeSomething conn = runRedis conn $ do
+--    ret <- lindex (pack "mylist") 0
+--    liftIO $ print ret
+
+writeSomething conn = runRedis conn $ do
+    ret <- switch nextIndex
+    liftIO $ print ret
+
+
+nextIndex :: Redis (Either Reply (Maybe Foo.ByteString))
+nextIndex = lindex (pack "mylist") (-1)
+
+switch :: Redis (Either Reply (Maybe Foo.ByteString)) -> Redis (Either Reply Integer)
+switch first_result = first_result >>= switch'
+
+switch' :: Either Reply (Maybe Foo.ByteString) -> Redis (Either Reply Integer)
+switch' (Left x) = return (Left x)
+switch' (Right Nothing) = rpush (pack "mylist") [(pack "0")] >>= switch''
+switch' (Right (Just bs)) = return (Right (read $ unpack bs :: Integer))
+
+switch'' :: Either Reply Integer -> Redis (Either Reply Integer)
+switch'' (Left x) = return (Left x)
+switch'' (Right _) = return (Right 0)
+
 
 
 
