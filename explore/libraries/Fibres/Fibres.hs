e67804fb7c931b16dd6c1a5d9fd6f95c50db74b3
Indika Piyasena <indika.piyasena@netboxblue.com>
2015-01-01 22:01:53 +1000

    hedis: refactoring

--- explore/libraries/Fibres/Fibres.hs
+++ explore/libraries/Fibres/Fibres.hs
@@ -18,15 +18,15 @@ data Search = Search {
 } deriving Show
 
 
-doSomething = persistThree "fibres" "mylistyoyo" "k1" "v1" "k2" "v2" "k3" "v3"
-
 
 sample_url = "http://www.gumtree.com.au/s-scooters/spring-hill-brisbane/scooter/k0c18629l3005758?price=0.00__1500.00"
 search = Search "gumtree" "scooter" sample_url
 
-
-
-
+persistSearch :: Search -> IO ()
+persistSearch search = persistThree "fibres" "search"
+                        "site" (site search)
+                        "keyword" (keyword search)
+                        "url" (url search)
 
 
 
--- explore/libraries/Fibres/HedisInterface.hs
+++ explore/libraries/Fibres/HedisInterface.hs
@@ -1,20 +1,21 @@
 module HedisInterface where
 
-
 import Database.Redis(Redis, Connection, Reply, runRedis, connect, defaultConnectInfo, hset, lindex, rpush)
-
 import Data.ByteString.Char8(pack, unpack)
+import Data.ByteString.Internal(ByteString)
 import Control.Monad.IO.Class(liftIO)
 
-import Data.ByteString.Internal as Foo(ByteString)
+
+createKey :: String -> Integer -> ByteString
+createKey prefix index = pack (prefix ++ ":" ++ (show index))
 
 
 
 persistThree prefix list_key k1 v1 k2 v2 k3 v3 = do
     conn <- connect defaultConnectInfo
     runRedis conn $ do
-        index <- (switch list_key) (nextIndex' list_key)
-        yo <- (incrementer list_key) index
+        index <- (switch list_key) (nextIndex list_key)
+        yo <- (incrementList list_key) index
         case yo of (Left reply) -> return (Left reply)
                    (Right idx) -> createSet' idx prefix k1 v1 k2 v2 k3 v3
     putStrLn "Done"
@@ -27,28 +28,29 @@ createSet' index prefix k1 v1 k2 v2 k3 v3 = do
     return ret
 
 
-createKey :: String -> Integer -> ByteString
-createKey prefix index = pack (prefix ++ ":" ++ (show index))
 
-nextIndex' :: String -> Redis (Either Reply (Maybe Foo.ByteString))
-nextIndex' list_key = lindex (pack list_key) (-1)
+nextIndex :: String -> Redis (Either Reply (Maybe ByteString))
+nextIndex list_key = lindex (pack list_key) (-1)
 
 
-switch :: String -> Redis (Either Reply (Maybe Foo.ByteString)) -> Redis (Either Reply Integer)
+switch :: String -> Redis (Either Reply (Maybe ByteString)) -> Redis (Either Reply Integer)
 switch list_key first_result = first_result >>= (switch' list_key)
 
-switch' :: String -> Either Reply (Maybe Foo.ByteString) -> Redis (Either Reply Integer)
+
+-- This is some sort of create if not exists
+switch' :: String -> Either Reply (Maybe ByteString) -> Redis (Either Reply Integer)
 switch' _ (Left x) = return (Left x)
 switch' list_key (Right Nothing) = rpush (pack list_key) [(pack "0")] >>= switch''
 switch' _ (Right (Just bs)) = return (Right (read $ unpack bs :: Integer))
 
+-- This is a fail or return the init state
 switch'' :: Either Reply Integer -> Redis (Either Reply Integer)
 switch'' (Left x) = return (Left x)
 switch'' (Right _) = return (Right 0)
 
-incrementer :: String -> Either Reply Integer -> Redis (Either Reply Integer)
-incrementer _ (Left x) = return (Left x)
-incrementer list_key (Right x) = rpush (pack list_key) [ (pack $ show (x + 1))]
+incrementList :: String -> Either Reply Integer -> Redis (Either Reply Integer)
+incrementList _ (Left x) = return (Left x)
+incrementList list_key (Right x) = rpush (pack list_key) [ (pack $ show (x + 1)) ]
 
 
 
